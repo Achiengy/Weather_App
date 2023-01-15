66383c4843be3545157a1ca79dd028541db773c2
Achiengy <petehope7@gmail.com>
2023-01-15 14:47:56 +0300

    UI: card,searchbar &  button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import './App.css';
+import Weather from '../src/components/weather';
 
 function App() {
   return (
     <div className="App">
-       
+       <Weather />
     </div>
   );
 }
